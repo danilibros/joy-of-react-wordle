63f353eb73ede3891224414718f70443d464eae7
Danielle Books <danielle@thirdiron.com>
2024-02-13 16:55:16 -0700

    exercise 4 - first pass

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
+import { checkGuess } from "../../game-helpers";
 import GuessInput from "../GuessInput";
 import GuessHistory from "../GuessHistory";
 
@@ -14,7 +15,12 @@ function Game() {
   const [guessHistoryList, setGuessHistoryList] = React.useState([]);
 
   function handleSubmitGuess(tentativeGuess) {
-    const nextGuess = { word: tentativeGuess, id: crypto.randomUUID() };
+    const guessStatus = checkGuess(tentativeGuess, answer);
+    const nextGuess = {
+      word: tentativeGuess,
+      id: crypto.randomUUID(),
+      status: guessStatus,
+    };
     setGuessHistoryList([...guessHistoryList, nextGuess]);
   }
 
